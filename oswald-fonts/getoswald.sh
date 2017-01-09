dd24888008ebfbe5e3a2b44e790ced9207b565e9
Dawid Zych <dawidzych@fastmail.fm>
2017-01-09 20:36:47 +0100

    Add getoswald.sh

---
+++ oswald-fonts/getoswald.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Get upstream zip and make source tar.gz
+
+ARCHIVE="oswald-fonts-4.0"
+TMPDIR=$(mktemp -d --tmpdir=/var/tmp getoswald-XXXXXXXXXX)
+[ $? != 0 ] && exit 1
+umask 022
+pushd "$TMPDIR"
+
+wget -N -O $ARCHIVE.zip https://fonts.google.com/download?family=Oswald
+unzip $ARCHIVE.zip -d $ARCHIVE
+tar -cvJf "$ARCHIVE.tar.xz" $ARCHIVE
+
+popd
+mv "$TMPDIR/$ARCHIVE.tar.xz" .
+rm -fr "$TMPDIR"
